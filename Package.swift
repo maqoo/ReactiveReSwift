6fd8cb41b5fffe2df738e8597dd62b887bf13355
Maciej Stramski <mstramski@gmail.com>
2020-09-10 12:25:24 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,67 @@
 // swift-tools-version:5.2
-
 import PackageDescription
 
 let package = Package(
     name: "ReactiveReSwift",
-    exclude: [
-      "Tests",
-      "Carthage",
-      "Docs"
+    products: [
+        .library(
+            name: "ReactiveReSwift",
+            targets: ["ReactiveReSwift-iOS"]),
+        .library(
+            name: "ReactiveReSwift",
+            targets: ["ReactiveReSwift-macOS"]),
+        .library(
+            name: "ReactiveReSwift",
+            targets: ["ReactiveReSwift-tvOS"]),
+        .library(
+            name: "ReactiveReSwift",
+            targets: ["ReactiveReSwift-watchOS"])
+    ],
+    targets: [
+        .target(
+            name: "ReactiveReSwift-iOS",
+            path: "Sources",
+            sources: ["."]
+        ),
+        .target(
+            name: "ReactiveReSwift-macOS",
+            path: "Sources",
+            sources: ["."]
+        ),
+        .target(
+            name: "ReactiveReSwift-tvOS",
+            path: "Sources",
+            sources: ["."]
+        ),
+        .target(
+            name: "ReactiveReSwift-watchOS",
+            path: "Sources",
+            sources: ["."]
+        ),
+
+        .testTarget(
+            name: "ReactiveReSwift-iOSTests",
+            dependencies: ["ReactiveReSwift-iOS"],
+            path: "Tests",
+            sources: ["."]
+        ),
+        .testTarget(
+            name: "ReactiveReSwift-macOSTests",
+            dependencies: ["ReactiveReSwift-macOS"],
+            path: "Tests",
+            sources: ["."]
+        ),
+        .testTarget(
+            name: "ReactiveReSwift-tvOSTests",
+            dependencies: ["ReactiveReSwift-tvOS"],
+            path: "Tests",
+            sources: ["."]
+        ),
+        .testTarget(
+            name: "ReactiveReSwift-watchOSTests",
+            dependencies: ["ReactiveReSwift-watchOS"],
+            path: "Tests",
+            sources: ["."]
+        ),
     ]
 )
